ee15e35516705636f242f0506071bb83874cea05
Luis Reyes <yuiftw05@gmail.com>
2024-10-04 19:46:14 -0600

    Initializing frontend and basic dependencies

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+// components
+import LandingPage from "./components/LandingPage/LandingPage";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage/>
+    </div>
+  );
+}
