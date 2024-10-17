fc47781357851903efa37aa160b9b2b5012bc3f1
Luis Reyes <yuiftw05@gmail.com>
2024-10-17 11:07:07 -0600

    feat: extracting users basic info from userInfo prop

--- server/src/controllers/userControllers/registerUserDb.js
+++ server/src/controllers/userControllers/registerUserDb.js
@@ -1,5 +1,14 @@
 import supabase from "../../db"
 
-export const registerUserDb = (userInfo, userTopSongs, userTopArtist)=> {
-
+export const registerUserDb = async (userInfo, userTopSongs, userTopArtist)=> {
+    try {
+        const {country, display_name, email, id, followers, uri, profile_photo, refresh_token } = userInfo
+        
+    } catch (error) {
+        console.log('error while storing user at databse', error)
+        return {
+            success: false,
+            error: error
+        }
+    }
 }
