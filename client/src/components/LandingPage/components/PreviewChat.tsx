d38bd5c948eab446d38d56b4843baf1614a2099c
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-10-23 09:46:17 -0400

    feat: se comento temporamente peticion a api por no uso

--- client/src/components/LandingPage/components/PreviewChat.tsx
+++ client/src/components/LandingPage/components/PreviewChat.tsx
@@ -1,5 +1,3 @@
-import { useAppSelector } from "@/redux/hooks";
-import { useGetPreviewChatsQuery } from "@/services/chatsApi";
 import Image from "next/image";
 
 interface chats {
@@ -40,13 +38,12 @@ const chats:chats[] = [
 	}
 ]
 const PreviewChat = () => {
-	const appToken = useAppSelector((state) => state.userReducer.appToken?.token);
-
-	// adelanto de la solicitud a la API
-  const { data } = useGetPreviewChatsQuery({
-    appToken,
-    page:0
-  })
+	// adelanto de la solicitud a la API (descomentar las lineas siguiente e impotar las librerias para implementar y usar variable data)
+	// const appToken = useAppSelector((state) => state.userReducer.appToken?.token);
+  // const { data } = useGetPreviewChatsQuery({
+  //   appToken,
+  //   page:0
+  // })
 
   return (
     <div className="flex flex-col gap-4 mt-8">
