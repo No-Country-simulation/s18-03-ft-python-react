580cb62c19d6ccd0e4f15e1738cfc63572f986a2
Luis Reyes <yuiftw05@gmail.com>
2024-10-27 02:48:20 -0600

    feat: using next image at coonect card component

--- client/src/components/Connect/ConnectCard.tsx
+++ client/src/components/Connect/ConnectCard.tsx
@@ -17,10 +17,12 @@ export default function ConnectCard({country, display_name, profile_photo, favGe
     <div className="bg-spotify-light-gray p-4 rounded-lg shadow-lg w-[100%] min-w-[280px] max-w-[450px] self-center md:self-start flex flex-col items-center gap-4">
       
       <div className="flex items-center space-x-4 self-start">
-        <img
-          className="w-16 h-16 rounded-full object-cover"
+        <Image
+          className="rounded-full object-cover"
           src={profile_photo}
           alt={`${display_name} profile`}
+          width={60}
+          height={40}
         />
         <div>
           <h3 className="text-white font-bold text-lg">{display_name}</h3>
