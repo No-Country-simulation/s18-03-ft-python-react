6fb4b843da2929b1ce2cd7e5d3105dea624d0eaa
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2024-10-29 17:10:19 -0300

    errores de eslint

--- client/src/components/Connect/ConnectCard.tsx
+++ client/src/components/Connect/ConnectCard.tsx
@@ -12,8 +12,10 @@ export default function ConnectCard({country, display_name, profile_photo}: prop
     <div className="bg-spotify-light-gray p-4 rounded-lg shadow-lg w-[100%] min-w-[280px] max-w-[450px] self-center md:self-start">
       
       <div className="flex items-center space-x-4">
-        <img
+        <Image
           className="w-16 h-16 rounded-full object-cover"
+          width={40}
+          height={40}
           src={profile_photo}
           alt={`${display_name} profile`}
         />
