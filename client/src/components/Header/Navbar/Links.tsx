c545188b4c3e8419261c22c3ac55d2a63f1e3b40
Luis Reyes <yuiftw05@gmail.com>
2024-10-30 03:06:01 -0600

    feat: rendering message icon and message in different devices

--- client/src/components/Header/Navbar/Links.tsx
+++ client/src/components/Header/Navbar/Links.tsx
@@ -1,5 +1,5 @@
 
-import { CiHome, CiChat1  } from "react-icons/ci";
+import { CiHome } from "react-icons/ci";
 import { FaUserFriends } from "react-icons/fa";
 
 
@@ -18,12 +18,6 @@ export const links: Link[] = [
     name: "Home",
     url: "/",
     icon: <CiHome className="h-6 w-6" />, 
-  },
-  {
-    id: 2,
-    name: "Message",
-    url: "/messages",
-    icon: <CiChat1 className="h-6 w-6" />,
   },
   {
     id: 3,
